92e62455d377026871ce0e0a6e6200875fd9ceb8
your <abdulmueedhassan0@gmail.com>
2025-01-14 22:38:01 -0800

    AH: rebase

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from 'antd'
+
+const App = () => {
+  return (
+    <div>
+      <Button color='default' variant='solid'>Click here</Button>
+    </div>
+  )
+}
+
+export default App
